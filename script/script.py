afa0ad55664b59ac281cd3ca7e7bd5d2c2079ca7
Yangjun Wang <yangjun.wan@luxus.fi>
2015-09-27 01:41:23 +0300

    remote execute install for workers

--- script/script.py
+++ script/script.py
@@ -7,19 +7,21 @@ if __name__ == "__main__":
 	config = json.load(open('cluster-config.json'))
 	# install git, clone repository, install jdk
 	for node in config['nodes']:
+		print("Install git on server %s" % node['ip'])
 		# install git
-		if '/usr/bin/git' not in subprocess.check_output(['whereis', 'git']):
-			subprocess.call(["sudo", "apt-get", "install", "-y", "git"])
+		if '/usr/bin/git' not in subprocess.check_output(["ssh", "cloud-user@"+node['ip'], 'whereis git']):
+			subprocess.call(["ssh", "cloud-user@"+node['ip'], "sudo apt-get install -y git"])
 			print("Install git")
 		# clone repository
-		files = subprocess.check_output(['ls','/home/cloud-user'])
+		files = subprocess.check_output(["ssh", "cloud-user@"+node['ip'], 'ls /home/cloud-user'])
 		if 'RealtimeStreamBenchmark' not in files:
-			subprocess.call(["git", "clone", "https://github.com/wangyangjun/RealtimeStreamBenchmark.git"])
+			subprocess.call(["ssh", "cloud-user@"+node['ip'], "git clone https://github.com/wangyangjun/RealtimeStreamBenchmark.git"])
 		else:
-			subprocess.Popen("git pull", shell=True, cwd="/home/cloud-user/RealtimeStreamBenchmark")
+			subprocess.Popen('ssh cloud-user@'+node['ip']+'"cd /home/cloud-user/RealtimeStreamBenchmark;git pull;"', shell=True)
+
 		# install jdk
-		subprocess.call(["python", "/home/cloud-user/RealtimeStreamBenchmark/script/install-jdk.py"])
+		subprocess.call(["ssh", "cloud-user@"+node['ip'], "python /home/cloud-user/RealtimeStreamBenchmark/script/install-jdk.py"])
 
 		# install storm
-		subprocess.call(["python", "/home/cloud-user/RealtimeStreamBenchmark/script/install-storm.py"])
+		subprocess.call(["ssh", "cloud-user@"+node['ip'], "python /home/cloud-user/RealtimeStreamBenchmark/script/install-storm.py"])
 
