c4235d6ba640b2c8d57c50c472f81a67ad1f4dcf
Yangjun Wang <yangjun.wan@luxus.fi>
2015-10-16 15:50:21 +0300

    update hostname and creat /mnt/anis-logs

--- script/update-hosts.py
+++ script/update-hosts.py
@@ -10,10 +10,9 @@ if __name__ == "__main__":
 	path = os.path.dirname(os.path.realpath(__file__))
 	config = json.load(open(path+'/cluster-config.json'));
 	for node in config['nodes']:
-		appendline('/etc/hosts', node['ip']+'\t'+node['host'])
+		appendline('/etc/hosts', node['ip']+'\t'+node['host'] + '\t'+node['hostname'])
 
 	appendline('/etc/hosts', '192.168.1.4\t'+'zoo1')
 
-	myip = get_ip_address()
-	mynode = [node for node in config['nodes'] if node['ip'] == myip][0]
-	appendline('/home/cloud-user/.bashrc', 'sudo hostname '+ mynode['host'])
+	subprocess.call(['sudo', 'mkdir', '-p', '/mnt/anis-logs'])
+	subprocess.call(['sudo', 'chown', '-R', 'cloud-user', '/mnt/anis-logs'])
