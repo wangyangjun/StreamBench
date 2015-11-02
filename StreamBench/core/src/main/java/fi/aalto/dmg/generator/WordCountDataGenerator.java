59a823a16fea3678cc398a102e8165c4d83c6950
Yangjun Wang <yangjun.wan@luxus.fi>
2015-11-02 17:47:20 +0200

    Update word count data generator

--- StreamBench/core/src/main/java/fi/aalto/dmg/generator/WordCountDataGenerator.java
+++ StreamBench/core/src/main/java/fi/aalto/dmg/generator/WordCountDataGenerator.java
@@ -1,6 +1,7 @@
 package fi.aalto.dmg.generator;
 
 import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.distribution.ZipfDistribution;
 import org.apache.commons.math3.random.*;
 import org.apache.log4j.Logger;
 
@@ -9,21 +10,30 @@ import org.apache.log4j.Logger;
  */
 public class WordCountDataGenerator {
     private static final Logger logger = Logger.getLogger(WordCountDataGenerator.class);
+    private static Integer SETANCE_NUM = 100;
+    private static Integer ZIPF_SIZE = 100000;
+    private static long ZIPF_EXPONENT = 1L;
 
     public static void main( String[] args ) {
-        RandomDataGenerator randomgenerator = new RandomDataGenerator();
-        randomgenerator.reSeed(100);
-        for (int i = 0; i < 100; i++) {
-            String l = randomgenerator.nextHexString(20);
-            logger.info(l);
+        RandomDataGenerator messageGenerator = new RandomDataGenerator();
+        messageGenerator.reSeed(1000000);
+
+        RandomDataGenerator wordGenerator = new RandomDataGenerator();
+        wordGenerator.reSeed(10000000000L);
+        // for loop to generate message
+        for (int i = 0; i < SETANCE_NUM; ++i) {
+            // message size
+            Double sentence_length_double = messageGenerator.nextGaussian(10, 1);
+            Integer sentence_length = sentence_length_double.intValue();
+            // generate word
+            StringBuilder messageBuilder = new StringBuilder();
+            for(int l = 0; l < sentence_length; ++l){
+                // get word length
+                Integer zipf = wordGenerator.nextZipf(ZIPF_SIZE, ZIPF_EXPONENT);
+                messageBuilder.append(Integer.toHexString(zipf).toUpperCase()).append(" ");
+            }
+            System.out.println(messageBuilder.toString());
         }
-
-        RandomGenerator randomGenerator = new JDKRandomGenerator();
-        randomGenerator.setSeed(17399225432l);  // Fixed seed means same results every time
-        UniformRandomGenerator uniformRandomGenerator = new UniformRandomGenerator(randomGenerator);
-        GaussianRandomGenerator rawGenerator = new GaussianRandomGenerator(randomGenerator);
-        NormalDistribution normalizedRandomGenerator = new NormalDistribution(randomGenerator, 1, 1);
-
 
     }
 }
