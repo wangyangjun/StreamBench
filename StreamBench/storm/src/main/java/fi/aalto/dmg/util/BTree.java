381652bc588104912ee41809ba1650ca4e9934e4
Yangjun Wang <yangjun.wang90@gmail.com>
2015-11-16 16:25:00 +0200

    Update BTree and add unit tests

--- StreamBench/storm/src/main/java/fi/aalto/dmg/util/BTree.java
+++ StreamBench/storm/src/main/java/fi/aalto/dmg/util/BTree.java
@@ -1,5 +1,7 @@
 package fi.aalto.dmg.util;
 
+import scala.Tuple2;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +11,25 @@ import java.util.List;
  * Created by jun on 16/11/15.
  */
 public class BTree<T> {
+    public static class Children{
+        private int child1;
+        private int child2;
+
+        public Children(int c1, int c2) {
+            this.child1 = c1;
+            this.child2 = c2;
+        }
+
+        public boolean contains(int index){
+            if(child1==index || child2==index) return true;
+            return false;
+        }
+
+        public String toString(){
+            return String.format("Child1:%d, Child2:%d", child1, child2);
+        }
+    }
+
     private List<T> dataContainer;
     private int size; // the number of leaves
 
@@ -27,7 +48,7 @@ public class BTree<T> {
         return findParent(nodeIndex, size);
     }
 
-    public int findParent(int nodeIndex, int leavesSize){
+    private int findParent(int nodeIndex, int leavesSize){
         // if root node
         if(isRoot(nodeIndex, leavesSize)) return nodeIndex;
         // if last leave node is odd node(0,1,2)
@@ -46,17 +67,18 @@ public class BTree<T> {
         else{
             // find parent of this node in a tmp tree
             // remove all leave nodes except last odd node
-            if(leavesSize%2==0){
+            if(leavesSize%2==0){ // remove all leaves
                 int tmpLeavesSize = leavesSize/2;
-                int tmpParent = findParent(nodeIndex-leavesSize, tmpLeavesSize);
-//                System.out.println(String.format("Tmp tree: size=%d, index=%d, parent=%d ", tmpLeavesSize, nodeIndex-leavesSize, tmpParent));
+                int tmpNodeIndex = nodeIndex-leavesSize;
+                int tmpParent = findParent(tmpNodeIndex, tmpLeavesSize);
+//                System.out.println(String.format("Tmp tree: size=%d, index=%d, parent=%d ", tmpLeavesSize, tmpNodeIndex, tmpParent));
                 return tmpParent + leavesSize;
-            } else {
+            } else { // remove leavesSize-1 leaves
                 // the last odd leave in old tree will be last leave in the new tree
                 int tmpLeavesSize = (leavesSize+1)/2;
                 int tmpNodeIndex = nodeIndex-leavesSize;
-                // if node is still not leave in the new tree
                 if( (nodeIndex+1)>=leavesSize+tmpLeavesSize ){ ++tmpNodeIndex; }
+
                 int tmpParent = findParent(tmpNodeIndex, tmpLeavesSize);
 //                System.out.println(String.format("Tmp tree: size=%d, index=%d, parent=%d ", tmpLeavesSize, tmpNodeIndex, tmpParent));
                 return tmpParent + leavesSize - 1;
@@ -64,6 +86,63 @@ public class BTree<T> {
         }
     }
 
+    public Children findChildren(int nodeIndex){
+        return findChildren(nodeIndex, size);
+    }
+
+    private Children findChildren(int nodeIndex, int leavesSize){
+        // if leave node, no child
+        if(nodeIndex < leavesSize){
+            return new Children(-1, -1);
+        }
+        // if it is on the second floor
+        if(nodeIndex < (leavesSize+leavesSize/2)){
+            // index in the new tree (remove one floor)
+            int tmpIndex = nodeIndex-leavesSize;
+            // get its children
+            return new Children(tmpIndex*2, tmpIndex*2+1);
+        } else {
+            // remove one floor
+            if(leavesSize%2==0){ // remove all leaves
+                int tmpLeavesSize = leavesSize/2;
+                int tmpNodeIndex = nodeIndex-leavesSize;
+                Children tmpChildren = findChildren(tmpNodeIndex, tmpLeavesSize);
+                Children children = new Children(tmpChildren.child1+leavesSize, tmpChildren.child2+leavesSize);
+                System.out.println(String.format("Tmp tree: size=%d, index=%d", tmpLeavesSize, tmpNodeIndex));
+                System.out.println("Children in tmp tree:" + tmpChildren);
+                System.out.println("Children in tree:" + children);
+                return children;
+            } else { // remove leavesSize-1 leaves
+                // the last odd leave in old tree will be last leave in the new tree
+                int tmpLeavesSize = (leavesSize+1)/2;
+                // nodeIndex couldn't on the second floor here
+                int tmpNodeIndex = nodeIndex-leavesSize+1;
+                Children tmpChildren = findChildren(tmpNodeIndex, tmpLeavesSize);
+                System.out.println(String.format("Tmp tree: size=%d, index=%d", tmpLeavesSize, tmpNodeIndex));
+                System.out.println("Children in tmp tree:" + tmpChildren);
+
+                // whether last odd node in children
+                if(tmpChildren.child1 > tmpLeavesSize-1){
+                    tmpChildren.child1 += leavesSize-1;
+                } else if(tmpChildren.child1 < tmpLeavesSize-1){
+                    tmpChildren.child1 += leavesSize;
+                } else {
+                    tmpChildren.child1 = leavesSize-1;
+                }
+
+                if(tmpChildren.child2 > tmpLeavesSize-1){
+                    tmpChildren.child2 += leavesSize-1;
+                } else if(tmpChildren.child2 < tmpLeavesSize-1){
+                    tmpChildren.child2 += leavesSize;
+                } else {
+                    tmpChildren.child2 = leavesSize-1;
+                }
+                System.out.println("Children in tree:" + tmpChildren);
+
+                return tmpChildren;
+            }
+        }
+    }
 
     public boolean isRoot(int nodeIndex){
         return isRoot(nodeIndex, size);
--- StreamBench/storm/src/test/java/fi/aalto/dmg/util/BTreeTest.java
+++ StreamBench/storm/src/test/java/fi/aalto/dmg/util/BTreeTest.java
@@ -32,11 +32,51 @@ public class BTreeTest {
         Assert.assertEquals(tree2.findParent(8), 16);
         Assert.assertEquals(tree2.findParent(11), 17);
         Assert.assertEquals(tree2.findParent(12), 18);
+        Assert.assertEquals(tree2.findParent(16), 20);
         Assert.assertEquals(tree2.findParent(17), 20);
         Assert.assertEquals(tree2.findParent(19), 21);
         Assert.assertEquals(tree2.findParent(20), 22);
-//        Assert.assertEquals(tree2.findParent(22), 22);
 
+        BTree<Integer> tree3 = new BTree<>(11);
+        System.out.println(tree3.findParent(10));
+        System.out.println(tree3.findParent(14));
+    }
+
+    @Test
+    public void findChildren(){
+        BTree<Integer> tree = new BTree<>(13);
+        Assert.assertTrue(tree.findChildren(13).contains(0));
+        Assert.assertTrue(tree.findChildren(13).contains(1));
+        Assert.assertTrue(tree.findChildren(17).contains(8));
+        Assert.assertTrue(tree.findChildren(17).contains(9));
+        Assert.assertTrue(tree.findChildren(19).contains(13));
+        Assert.assertTrue(tree.findChildren(19).contains(14));
+        Assert.assertTrue(tree.findChildren(21).contains(17));
+        Assert.assertTrue(tree.findChildren(21).contains(18));
+        Assert.assertTrue(tree.findChildren(22).contains(19));
+        Assert.assertTrue(tree.findChildren(22).contains(20));
+        Assert.assertTrue(tree.findChildren(23).contains(21));
+        Assert.assertTrue(tree.findChildren(23).contains(12));
+        Assert.assertTrue(tree.findChildren(24).contains(22));
+        Assert.assertTrue(tree.findChildren(24).contains(23));
+
+        BTree<Integer> tree2 = new BTree<>(12);
+        Assert.assertTrue(tree2.findChildren(13).contains(2));
+        Assert.assertTrue(tree2.findChildren(13).contains(3));
+        Assert.assertTrue(tree2.findChildren(15).contains(6));
+        Assert.assertTrue(tree2.findChildren(15).contains(7));
+        Assert.assertTrue(tree2.findChildren(18).contains(12));
+        Assert.assertTrue(tree2.findChildren(18).contains(13));
+        Assert.assertTrue(tree2.findChildren(20).contains(16));
+        Assert.assertTrue(tree2.findChildren(20).contains(17));
+        Assert.assertTrue(tree2.findChildren(21).contains(18));
+        Assert.assertTrue(tree2.findChildren(21).contains(19));
+        Assert.assertTrue(tree2.findChildren(22).contains(21));
+        Assert.assertTrue(tree2.findChildren(22).contains(20));
+
+        BTree<Integer> tree3 = new BTree<>(10);
+        Assert.assertTrue(tree3.findChildren(18).contains(14));
+        Assert.assertTrue(tree3.findChildren(18).contains(17));
 
     }
 }
