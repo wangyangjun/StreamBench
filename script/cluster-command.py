007edf3e3fb8d8c8a2138af78560a7580561739f
Yangjun Wang <yangjun.wang90@gmail.com>
2015-12-16 22:18:57 +0200

    import os in cluster-command.py

--- script/cluster-command.py
+++ script/cluster-command.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import subprocess
 import sys
+import os
 import json
 from util import appendline, get_ip_address
 
@@ -9,11 +10,11 @@ from util import appendline, get_ip_address
 if __name__ == "__main__":
 	# start server one by one
 	if len(sys.argv) < 2:
-		sys.stderr.write('Usage: python %s "command"\n' % (sys.argv[0]))
-		sys.stderr.write("cd /home/cloud-user/StreamBench;git checkout .;git pull;")
-		sys.stderr.write("nohup bash /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties")
-		sys.stderr.write("nohup bash /usr/local/kafka/bin/kafka-server-stop.sh")
-		sys.stderr.write("nohup java -cp /home/cloud-user/StreamBench/jars/data-generator.jar fi.aalto.dmg.generator.WordCountDataGenerator &")
+		print('Usage: python %s "command"\n' % (sys.argv[0]))
+		print("cd /home/cloud-user/StreamBench;git checkout .;git pull;")
+		print("nohup bash /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties")
+		print("nohup bash /usr/local/kafka/bin/kafka-server-stop.sh")
+		print("nohup java -cp /home/cloud-user/StreamBench/jars/data-generator.jar fi.aalto.dmg.generator.WordCountDataGenerator &")
 		sys.exit(1)
 	else:
 		path = os.path.dirname(os.path.realpath(__file__))
