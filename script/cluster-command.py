dd2ace5a4c1fa88b09b8a24f2e39361cd39483e9
Yangjun Wang <yangjun.wang90@gmail.com>
2016-01-18 16:07:06 +0200

    update cluster-command.py and data-generator frequency

--- script/cluster-command.py
+++ script/cluster-command.py
@@ -9,8 +9,10 @@ from util import appendline, get_ip_address
 
 if __name__ == "__main__":
 	# start server one by one
-	if len(sys.argv) < 2:
-		print('Usage: python %s "command"' % (sys.argv[0]))
+	servers = ['all', 'kafka', 'worker']
+	if len(sys.argv) < 3 or sys.argv[1] not in servers:
+		print('Usage: python %s " servers command"' % (sys.argv[0]))
+		print('servers could be in "all", "kafka", "worker"')
 		print("cd /home/cloud-user/StreamBench;git checkout .;git pull;")
 		print("nohup bash /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties")
 		print("nohup bash /usr/local/kafka/bin/kafka-server-stop.sh")
@@ -19,6 +21,14 @@ if __name__ == "__main__":
 	else:
 		path = os.path.dirname(os.path.realpath(__file__))
 		config = json.load(open(path+'/cluster-config.json'))
-		for node in config['nodes']:
-			subprocess.Popen(['ssh', 'cloud-user@'+node['ip'], sys.argv[1]])
-		
+		if sys.argv[1] == 'all':
+			for node in config['nodes']:
+				subprocess.Popen(['ssh', 'cloud-user@'+node['ip'], sys.argv[1]])
+		elif sys.argv[1] == 'kafka':
+			for node in config['nodes']:
+				if node['kafka']:
+					subprocess.Popen(['ssh', 'cloud-user@'+node['ip'], sys.argv[1]])
+		elif sys.argv[1] == 'worker':
+			for node in config['nodes']:
+				if node['kafka'] == False:
+					subprocess.Popen(['ssh', 'cloud-user@'+node['ip'], sys.argv[1]])
