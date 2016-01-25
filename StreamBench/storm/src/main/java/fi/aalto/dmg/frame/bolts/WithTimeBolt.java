900b3c084f1ed795e4de7b835210458a53fb849c
Yangjun Wang <yangjun.wang90@gmail.com>
2016-01-25 22:44:12 +0200

    update data generator speed

--- StreamBench/storm/src/main/java/fi/aalto/dmg/frame/bolts/WithTimeBolt.java
+++ StreamBench/storm/src/main/java/fi/aalto/dmg/frame/bolts/WithTimeBolt.java
@@ -6,6 +6,7 @@ import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
+import fi.aalto.dmg.util.Constant;
 import fi.aalto.dmg.util.WithTime;
 
 /**
@@ -15,7 +16,18 @@ public class WithTimeBolt<T> extends BaseBasicBolt {
 
     @Override
     public void execute(Tuple input, BasicOutputCollector collector) {
-        collector.emit(new Values(new WithTime<T>((T)input.getValue(0), System.currentTimeMillis())));
+        if(input.getValue(0) instanceof String){
+            String str = (String) input.getValue(0);
+            String[] list = str.split(Constant.TimeSeparatorRegex);
+            if(list.length == 2) {
+                collector.emit(new Values(new WithTime<T>((T)list[0], Long.parseLong(list[1]))));
+
+            } else {
+                collector.emit(new Values(new WithTime<T>((T) input.getValue(0), System.currentTimeMillis())));
+            }
+        } else {
+            collector.emit(new Values(new WithTime<T>((T) input.getValue(0), System.currentTimeMillis())));
+        }
     }
 
     @Override
