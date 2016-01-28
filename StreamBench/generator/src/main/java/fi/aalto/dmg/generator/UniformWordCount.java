5b89eb389205182eb8455d729d97c058e85935d7
Yangjun Wang <yangjun.wang90@gmail.com>
2016-01-28 10:49:41 +0200

    update generator

--- StreamBench/generator/src/main/java/fi/aalto/dmg/generator/UniformWordCount.java
+++ StreamBench/generator/src/main/java/fi/aalto/dmg/generator/UniformWordCount.java
@@ -32,7 +32,7 @@ public class UniformWordCount {
         // 10  ---- 6K/s
         // 50  ---- 15K/s
         // 100 ---- 27K/s
-        int SLEEP_FREQUENCY = 100;
+        int SLEEP_FREQUENCY = 80;
         if(args.length > 0) {
             SLEEP_FREQUENCY = Integer.parseInt(args[0]);
         }
